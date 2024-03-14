8dc044060ae5cf302df6e1a6c0bf695949bf51f6
muginga72 <lmj.muginga@gmail.com>
2024-03-14 16:03:13 -0400

    R4RApp initial commit pushed to main repo!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_admin!
   # before_action :authenticate_user!
-  # before_action :redirect_signed_in_admin
+  # before_action :redirect_sign_out_user
   # before_action :redirect_signed_in_user
   
   # private
@@ -10,7 +10,13 @@ class ApplicationController < ActionController::Base
   #   redirect_to authenticated_root_path if admin_signed_in?
   # end
 
-  # def redirect_signed_in_user
-  #   redirect_to authenticated_root_path if user_signed_in?
+  # def redirect_sign_out_user
+  #   redirect_to rooms_path if user_sign_out?
   # end
+  helper_method :current_user
+
+  def current_user
+    # If session[:user_id] is nil, set it to nil, otherwise find the user by id.
+    @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+  end
 end
